ed5444f48641bc9b92ee25bfd457df7c1da785fd
Aymeric Collange <aymeric-brice.collange@ensimag.grenoble-inp.fr>
2016-10-05 13:19:09 +0200

    initial commi

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /user/1/.base/collanga/home/sepc/2B/ensimag-shell
+# Build directory: /user/1/.base/collanga/home/sepc/2B/ensimag-shell/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(UnitShellTests "../tests/allShellTests.rb")
